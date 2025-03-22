08ac546dd8cc8336d9602503c1b95d963058ab71
AY_o101o <ayoakinniyi10@gmail.com>
2025-03-22 15:54:30 +0000

    update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,7 +3,7 @@ import { Menu, Search, UserRound, ShoppingBag } from "lucide-react"
 export default function Navbar(){
 
     return(
-        <nav className="flex items center bg-gray-800 text-sky-50 w-full py-3 px-5 justify-between">
+        <nav className="fixed top-0 left-0 z-50 flex items center bg-gray-800 text-sky-50 w-full py-3 px-5 justify-between">
                 <div className="flex text-xl font-bold space-x-3">
                     <Menu/>
                     <h1>DeChipest</h1>
