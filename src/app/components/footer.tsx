981a8b0204eac06dc1a34c6dab14d54bad540d59
AY_o101o <ayoakinniyi10@gmail.com>
2025-03-24 16:55:39 +0000

    update

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <p className="font-bold text-5xl mb-5">dechipest</p>
         <p>
           Got questions? Call us 24/7! <br />
-          <span className="text-3xl">
+          <span className="text-2xl">
             +234 806 789 0534 <br /> support@dechipest.com
           </span>
         </p>
