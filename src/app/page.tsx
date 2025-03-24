1ba4aa1efd6b9db5e248b59edcd86eb815d366fe
AY_o101o <ayoakinniyi10@gmail.com>
2025-03-24 14:08:42 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-gray-200 text-gray-800">
-        <h1 className="text-4xl px-8 pt-8 pb-40">
+        <h1 className="text-4xl px-8 pt-8 pb-30">
           BEST DEALS <br />
           IN NIGERIA <br />
           <span className="font-bold text-lg">
@@ -38,6 +38,7 @@ export default function Home() {
           </span>
         </h1>
       </div>
+      <div className=""></div>
     </div>
   );
 }
